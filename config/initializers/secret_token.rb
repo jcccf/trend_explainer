a337eb2886d3e181f5ab38779dc65fb938dab1d0
Justin Cheng <jccccf@gmail.com>
2011-03-05 18:02:39 -0500

    New Rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TrendExplainer::Application.config.secret_token = '23fb1aeb4396f932bd080f40c00b0d1d511c85aa40ab0f5f98ab3717ddbb620029a3f1f429bf9890dbd42d5434dda543d444c64e8f58567f39f2d712f34e07ad'
---
+++ texplainer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Texplainer::Application.config.secret_token = '4b622284ff6b0a12c051e71800d27b9645b94f84222fac7bbeb53a3477e3a5cdac40c89f8589b7cc6a2326ad788f52ddcc3239531e5cb3f81e7033bf348943ee'
